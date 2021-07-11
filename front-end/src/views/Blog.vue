16cf5032cf27753c68b31e54fd86c8446c9e1c2b
[Max <[rankinsm@wit.edu]>
2021-07-11 19:31:46 -0400

    final-v1

---
+++ front-end/src/views/Blog.vue
@@ -0,0 +1,30 @@
+<template>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <div id="container-blog">
+        <h1>
+            Blog
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Blog"
+}
+</script>
+
+<style scoped>
+     #container-blog {
+        background-color: black;
+        height: 100%;
+        margin-top: 0%;
+        padding: 3%;
+    }
+
+    #{
+        font-size: 36px;
+        color: white;
+        text-align: center;
+        font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    }
+</style>

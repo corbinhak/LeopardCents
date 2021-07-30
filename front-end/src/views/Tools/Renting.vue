eedf6785b08858918ae8f8b3dff7c3c710641e09
rankinsm <rankinsm@wit.edu>
2021-07-30 18:23:40 -0400

    rent style

--- front-end/src/views/Tools/Renting.vue
+++ front-end/src/views/Tools/Renting.vue
@@ -67,11 +67,60 @@ export default {
 
 <style scoped>
     .red {
+        background-color: #E7202B;
         margin-top: 0%;
         padding: 3%;
         padding-left: 15%;
         padding-right: 15%;
-        background-color: #E7202B;
+        height: 525px;
+    }
+    
+    h1 {
+        text-align: center;
+        padding-bottom: 20px;
+    }
+
+
+
+    table{
+        width: 100%;
+        align-self: center;
+    }
+
+    th{
+        padding-bottom: 30px;
+    }
+
+    input{
+        font-size: 18px;
+    }
+
+    #submit{
+        background-color:  #05B13F;
+        border: 5px solid #05B13F;
+        border-radius: 5px;
+        color: white;
+        font-size: 18px;
+        width: 150px;
+        margin-left: 40%;
+    }
+
+    label {
+        font-size: 20px;
+        text-align: right;
+        width: 100%;
+
+
+    }
+
+    td{
+        padding-top: 20px;
+        font-size: 20px;
+    }
+
+    h4{
         text-align: left;
+        margin-left: -20px;
+        font-size: 20px;
     }
 </style>

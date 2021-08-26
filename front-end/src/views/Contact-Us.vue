8dd0afa52f2c2130e25a474e3c8fc1b8627300d5
Corbin Hakimian <hakimianc@wit.edu>
2021-08-26 00:26:45 -0400

    Add netlify forms

--- front-end/src/views/Contact-Us.vue
+++ front-end/src/views/Contact-Us.vue
@@ -6,7 +6,7 @@
         <p id="cu-p">
             Have any questions or concern? Feel free to send us a message!
         </p>
-        <form name="Leopard Cents Contact Us" netlify>
+        <form name="Leopard Cents Contact Us" method="POST" data-netlify="true">
             <div>
                 <div id="cu-ul" >
                     
